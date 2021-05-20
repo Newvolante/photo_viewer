e74c239ac025c3bb9a9af821d30f13812c8fcb59
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-05-20 12:29:15 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+$(function() {
+
+    // event listener for a elements
+    $('#links').on('click', 'a', function(e) {
+        
+        if ($('#image')) {
+            $('#image').remove();
+        }
+        
+        e.preventDefault();
+        console.log('Miniature clicked');
+        
+        $href = $(this).attr('href');
+
+
+        console.log('href link :' + $href);
+        $imgElement = '<img id="image" src="./images/' + $href + '.png"/>';
+
+        $('#photo-container').append($imgElement);
+        
+    });
+});
